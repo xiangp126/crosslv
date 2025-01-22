3526111089a295c5d4bf0203bfb5367ad6d2ab00
Peter X <hi.pxiang@gmail.com>
2025-01-21 16:27:21 -0800

    support long options and add completion script for jmake

---
+++ completion-files/jmake_completion.bash
@@ -0,0 +1,104 @@
+#!/bin/bash
+# compgen filters the word list based on what the user has already typed ($cur)
+# COMPREPLY is an array variable
+# COMPREPLY stores the list of possible completion suggestions that will be shown to the user when they press the Tab key.
+# COMP_WORDS is an array that contains all the words in the current command line.
+# COMP_CWORD is an integer that represents the index of the word that the user is currently typing or trying to complete.
+# The -- in a command is used to signal the end of options, ensuring that subsequent arguments are treated as positional parameters
+# rather than options, even if they start with a dash (-).
+
+# Completion function for jmake
+_jmake_complete() {
+    local cur prev opts long_opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # List of all short options
+    opts="-h -m -w -j -c -C -b -B -o -t -s -P -p -u -l -k -O"
+
+    # List of all long options
+    long_opts="--help --model --working-dir --jobs --clean --configure --bear \
+               --build-target --build --bear-remove --max-build-attempt \
+               --target --sync-file --sync-port --username --password --kernel \
+               --disable-optimization"
+
+    # Handle option arguments
+    case $prev in
+        # Build options
+        -m|--model)
+            # You can customize this list based on your available models
+            local models="FGT_VM64_KVM VMWARE"
+            COMPREPLY=( $(compgen -W "${models}" -- ${cur}) )
+            return 0
+            ;;
+        -w|--working-dir)
+            # Directory completion
+            COMPREPLY=( $(compgen -d -- ${cur}) )
+            return 0
+            ;;
+        -j|--jobs)
+            # Suggest some common numbers of jobs
+            local jobs="1 2 4 8 16"
+            COMPREPLY=( $(compgen -W "${jobs}" -- ${cur}) )
+            return 0
+            ;;
+        -B|--build-target)
+            # Common build targets
+            local targets="image.out"
+            COMPREPLY=( $(compgen -W "${targets}" -- ${cur}) )
+            return 0
+            ;;
+        -T|--max-build-attempt)
+            # Suggest some common numbers for max attempts
+            local attempts="1 2 3"
+            COMPREPLY=( $(compgen -W "${attempts}" -- ${cur}) )
+            return 0
+            ;;
+        # Sync options
+        -t|--target)
+            # Common FortiGate targets
+            local targets="fgt1 fgt2 fpx1 fpx3"
+            COMPREPLY=( $(compgen -W "${targets}" -- ${cur}) )
+            return 0
+            ;;
+        -s|--sync-file)
+            # File completion with .out extension
+            COMPREPLY=( $(compgen -f -X '!*.out' -- ${cur}) )
+            return 0
+            ;;
+        -P|--sync-port)
+            # Common SSH ports
+            local ports="22 8822 8022"
+            COMPREPLY=( $(compgen -W "${ports}" -- ${cur}) )
+            return 0
+            ;;
+        -l|-u|--username)
+            # Common FortiGate usernames
+            local users="admin corsair"
+            COMPREPLY=( $(compgen -W "${users}" -- ${cur}) )
+            return 0
+            ;;
+        -p|--password)
+            # For security reasons, don't suggest passwords
+            return 0
+            ;;
+    esac
+
+    # Handle initial options
+    if [[ ${cur} == -* ]]; then
+        # If it starts with --, only suggest long options
+        if [[ ${cur} == --* ]]; then
+            COMPREPLY=( $(compgen -W "${long_opts}" -- ${cur}) )
+        else
+            # Suggest both short and long options
+            # COMPREPLY=( $(compgen -W "${opts} ${long_opts}" -- ${cur}) )
+            # Suggest only short options
+            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        fi
+        return 0
+    fi
+}
+
+# Register the completion function for jmake
+complete -F _jmake_complete jmake
