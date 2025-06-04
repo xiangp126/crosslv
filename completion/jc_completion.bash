f5a4f4d2023ab7b7aefdb1f1decde7b710f41b06
Peter X <hi.pxiang@gmail.com>
2025-06-04 14:04:39 -0700

    add opengrok option

--- completion/jc_completion.bash
+++ completion/jc_completion.bash
@@ -10,7 +10,8 @@ _jc_complete() {
     long_opts="--help --no-tools --debug --clangd --link-clang-format --link-nodejs \
                --auto-remove --upgrade --docker --insecure --chinese-pinyin --prerequisite \
                --chinese-pinyin-force --vnc-start --vnc-stop --unlock-vnc --lock-vnc \
-               --vnc-restart --vnc --firefox-deb --update"
+               --vnc-restart --vnc --firefox-deb --update --opengrok-start --opengrok-stop \
+               --opengrok-restart --opengrok --opengrok-indexer"
 
     if [[ ${cur} == -* ]]; then
         # If it starts with --, only suggest long options
