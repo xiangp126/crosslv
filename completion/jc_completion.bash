db8e63cb2029f0f85703f4ece3f4f1b7bc978e2a
Peter X <hi.pxiang@gmail.com>
2025-06-26 16:09:04 -0700

    update the vnclock options

--- completion/jc_completion.bash
+++ completion/jc_completion.bash
@@ -1,18 +1,27 @@
 # completion.bash for oneKey.sh
 
 _jc_complete() {
-    local cur opts long_opts
+    local cur prev opts long_opts
     COMPREPLY=() # Array that will hold the completions
     cur="${COMP_WORDS[COMP_CWORD]}" # Current cursor position
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     # Define command options
     opts="-h -n -d -f"
     long_opts="--help --no-tools --debug --clangd --link-clang-format --nvm --wireshark \
                --auto-remove --upgrade --docker --prerequisite --chinese-pinyin \
-               --vnc-start --vnc-stop --unlock-vnc --lock-vnc --vnc-restart --vnc \
+               --vnc --vnc-start --vnc-stop --vnc-restart --unlock-vnc --lock-vnc --vnclock \
                --firefox --update --opengrok-start --opengrok-stop --opengrok-restart \
                --opengrok --opengrok-indexer --samba --samba-bypass-password --git-lfs \
                --litellm-start --litellm-stop --litellm-restart --litellm"
+
+    case "${prev}" in
+        --vnclock)
+            local vnclock_options="unlock lock status"
+            COMPREPLY=( $(compgen -W "${vnclock_options}" -- ${cur}) )
+            return 0
+            ;;
+    esac
 
     if [[ ${cur} == -* ]]; then
         # If it starts with --, only suggest long options
