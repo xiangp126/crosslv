8f51cf0eb51d4019ffad6090267124df7afd1661
Peter X <hi.pxiang@gmail.com>
2026-05-31 12:13:05 +0800

    add --bbr

--- completion/jc_completion.bash
+++ completion/jc_completion.bash
@@ -94,6 +94,7 @@ _jc_complete() {
                --cursor-backup --cursor-restore --singbox --singbox-xray --singbox-xray-autossh --singbox-wg \
                --xray --xray-port --xray-server --xray-remove \
                --sslh --sslh-ssh-port --sslh-xray-port --sslh-status --sslh-rollback --sslh-remove \
+               --bbr --bbr-status \
                --wg --wg-port --wg-server --wg-client --wg-remove \
                --rtsp --rtsp-all --rtsp-kill --rtsp-list --rtsp-raw --rtsp-1080 \
                --rtsp-stream --rtsp-ip --rtsp-resolution"
